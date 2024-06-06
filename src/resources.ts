be45ecb5cd87ee45b69e30811f282e22009d0896
cauazinhho <emerson.cauarodrigues@gmail.com>
2024-06-06 17:21:23 -0300

    Feat: Gamification feito

--- src/resources.ts
+++ src/resources.ts
@@ -1,13 +1,15 @@
 import { ImageSource, Loader } from "excalibur";
 import sword from "./images/sword.png";
 import logo from "./images/logo.png";
-import logoVertical from "./images/logo-vertical.png"
+import logoVertical from "./images/logo-vertical.png";
+import furia from "./images/furia.png"
 
 
 export const Resources = {
   Sword: new ImageSource(sword),
   Logo: new ImageSource(logo),
-  LogoVertical: new ImageSource(logoVertical)
+  LogoVertical: new ImageSource(logoVertical),
+  Furia: new ImageSource(furia)
 
 } as const;
 
--- src/scenes/gamificationScene.ts
+++ src/scenes/gamificationScene.ts
@@ -1,7 +1,46 @@
-import { Color, Engine, Scene } from "excalibur";
+import { Actor, Color, Engine, Scene, vec } from "excalibur";
+import { Resources } from "../resources";
 
-export class gamificationScene extends Scene{
+export class gamificationScene extends Scene {
+    elementoHTML?: HTMLElement
+
     onInitialize(engine: Engine<any>): void {
-        this.backgroundColor = Color.Chartreuse
+        this.backgroundColor = Color.fromHex("#403f4c")
+
+
+        this.elementoHTML = document.createElement("div") as HTMLElement
+        this.elementoHTML.style.opacity = "1"
+
+        let containerGame = document.querySelector(".container-game") as HTMLElement
+        containerGame?.appendChild(this.elementoHTML)
+
+        this.elementoHTML.classList.add(".sobre-gamificacao")
+
+        this.elementoHTML.innerHTML = `<h2>O que é gamificação?</h2>
+    
+        <p>Gamificação é a aplicação de elementos típicos de jogos em contextos não lúdicos, com o objetivo de engajar e motivar indivíduos a atingir determinados objetivos. Esta abordagem se utiliza de componentes como pontuação, níveis, recompensas, desafios, e feedback imediato, visando promover comportamentos desejados e aumentar a participação e o comprometimento dos participantes.</p>`
+
+        this.elementoHTML.classList.add("gamificacao")
+
+
+        
+
+        //Carregando a imagem do logo
+        let imagemFuria = Resources.Furia.toSprite()
+
+        //Definir o tamanho da imagem
+        imagemFuria.scale = vec(0.7, 0.7)
+
+        //Adicionar imagem do Actor
+        let actorFuria = new Actor ({
+            pos: vec(300, engine.halfDrawHeight)
+        })
+
+        actorFuria.graphics.add(imagemFuria)
+
+        //Rederizar actor na tela/cena
+        this.add(actorFuria)
+
     }
+
 }
